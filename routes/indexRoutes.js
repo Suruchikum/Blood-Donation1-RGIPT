723ad51ab9a9aa6616e5c51f1a0497e4c3f17fe0
Suruchikum <Suruchikum>
2024-10-19 20:12:21 +0530

    removed comments

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,42 +2,29 @@ const express = require("express");
 const router = express.Router();
 const User = require("../modals/user.modals");
 const jwt = require("jsonwebtoken");
-// const verifyToken = require('../middleware/authMiddleware');
-const authMiddleware = require("../middleware/authMiddleware");
 
+const authMiddleware = require("../middleware/authMiddleware");
 
 const {
   handleLogin,
   handleRegister,
- 
+
   handleLogout,
 } = require("../controllers/indexController");
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
-router.get("/logout" , handleLogout);
+router.get("/logout", handleLogout);
 
 router.get("/register", (req, res) => {
   res.render("register");
 });
-// router.get("/help", (req, res) => {
-//   res.render("help");
-// });
-
-
-
 
 router.post("/login", handleLogin);
-// router.post("/logout" , handleLogout);
+
 router.post("/register", handleRegister);
 
-// router.post("/help", handleHelp);
-
-
-// router.get('/someRoute', (req, res) => {
-// //   // Your route handler code
-//  });
 router.use((req, res, next) => {
   next();
 });
